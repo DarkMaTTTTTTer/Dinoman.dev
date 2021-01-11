b59b224d99ebbe7f5c54fb63d5fbfeb0e5ec34d9
DarkMaTTTTTTer <61645374+DarkMaTTTTTTer@users.noreply.github.com>
2021-01-11 13:11:08 -0800

    Add files via upload

---
+++ Current-Projects.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Dinoman.dev</title>
+    <meta name="description" content="Welcome, Come on in.">
+    <link rel="icon" type="image/png" sizes="400x352" href="/assets/img/logo2.png?h=6ad3b23f7a0be358783adb294da3d091">
+    <link rel="icon" type="image/png" sizes="400x352" href="/assets/img/logo2.png?h=6ad3b23f7a0be358783adb294da3d091">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css?h=4f10ccc55b166919047815d3c65de7fb">
+    <link rel="stylesheet" href="/assets/fonts/font-awesome.min.css?h=9db842b3dc3336737559eb4abc0f1b3d">
+    <link rel="stylesheet" href="/assets/css/Animated-CSS-Waves-Background-SVG.css?h=663274753fd79fac442fe55a227b70e7">
+    <link rel="stylesheet" href="/assets/css/Article-List.css?h=cec575f25bba55cfaa10100750e76412">
+    <link rel="stylesheet" href="/assets/css/Contact-Form-Clean.css?h=c942239c91f94a6b90d67fb2496c21bf">
+    <link rel="stylesheet" href="/assets/css/gradient-navbar-1.css?h=b3bbc83d97bd59482a9960d0ecc55a15">
+    <link rel="stylesheet" href="/assets/css/gradient-navbar.css?h=b3bbc83d97bd59482a9960d0ecc55a15">
+    <link rel="stylesheet" href="/assets/css/Highlight-Blue.css?h=91a04f2b18410bc09760f3473788176a">
+    <link rel="stylesheet" href="/assets/css/Highlight-Clean.css?h=22f5c37d9256feae966552e68b9ff2f3">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="/assets/css/styles.css?h=d41d8cd98f00b204e9800998ecf8427e">
+    <link rel="stylesheet" href="/assets/css/Swiper-Slider-Card-For-Blog-Or-Product-1.css?h=ffee04c5cb354382dfa8f41065ecb54c">
+    <link rel="stylesheet" href="/assets/css/Swiper-Slider-Card-For-Blog-Or-Product.css?h=35808ef06463b831c589a6910b5c8506">
+</head>
+
+<body style="background: #222222;color: rgb(255,255,255);">
+    <nav class="navbar navbar-dark navbar-expand-md" data-aos="fade-up" data-aos-once="true" id="app-navbar">
+        <div class="container-fluid"><a class="navbar-brand" href="https://dinoman.dev"><i class="fa fa-connectdevelop" id="brand-logo"></i></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link active" href="https://github.com/DarkMaTTTTTTer/">Github</a></li>
+                    <li class="nav-item"><a class="nav-link" href="https://dinoman.dev/Current-Projects.php">Current Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" href="https://dinoman.dev/Contact.php">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div data-aos="fade-up" data-aos-once="true" class="highlight-blue" style="background: #222222;">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Current Projects:</h2>
+                <p class="text-center" style="color: rgb(255,255,255);">Here you can find my current work. You can also find links to the demo versions of these projects.</p>
+            </div>
+            <div class="buttons"></div>
+        </div>
+    </div>
+    <div data-aos="fade-up" data-aos-once="true" class="highlight-blue" style="background: #222222;">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">"The Big Ones"</h2>
+            </div>
+            <div class="buttons"></div>
+        </div>
+        <div class="blog-slider" style="background: #222222;">
+            <div class="blog-slider__wrp swiper-wrapper">
+                <div class="blog-slider__item swiper-slide">
+                    <div></div>
+                    <div class="blog-slider__img"><img src="/assets/img/logo2.png?h=6ad3b23f7a0be358783adb294da3d091"></div>
+                    <div class="blog-slider__content"><span class="blog-slider__code" style="color: rgb(255,255,255);">October 13, 2020</span><div class="blog-slider__title">Dinoman.dev</div><div class="blog-slider__text">Dinoman.dev is a development site that allows for live testing of applications. This page contains information on all my other projects.</div><a class="class=&quot;blog-slider__button" href="https://dinoman.dev">Dinoman.dev</a></div>
+                </div>
+                <div class="blog-slider__item swiper-slide">
+                    <div></div>
+                    <div class="blog-slider__img"><img src="/assets/img/LinuxNodes.net.png?h=a20b144734ef0e40cdf5ea2ba5f16357"></div>
+                    <div class="blog-slider__content"><span class="blog-slider__code">December 28, 2018</span><div class="blog-slider__title">LinuxNodes.net</div><div class="blog-slider__text">LinuxNodes is a hosting platform that specialises in free member hosting.</div><a class="class=&quot;blog-slider__button" href="https://linuxnodes.net">LinuxNodes.net</a></div>
+                </div>
+                <div class="blog-slider__item swiper-slide">
+                    <div></div>
+                    <div class="blog-slider__img"><img src="/assets/img/comming%20soon.png?h=c6db64a07861d1fba38af9dbee0e190a"></div>
+                    <div class="blog-slider__content"><span class="blog-slider__code">Comming June 2020</span><div class="blog-slider__title">Comming Soon...</div><div class="blog-slider__text">Who Knows, mabey its gordan ramsey on a stick!</div><a data-toggle="tooltip" data-bs-tooltip="" class="class=&quot;blog-slider__button" href="https://dinoman.dev/youfoundme" title="Nothing To See Here...">Not Yet...</a></div>
+                </div>
+                <div class="blog-slider__pagination"></div>
+            </div>
+        </div>
+    </div>
+    <div data-aos="fade-up" data-aos-once="true" class="highlight-blue" style="background: #222222;">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Applications</h2>
+                <p class="text-center" style="color: rgb(255,255,255);">Dinoman.dev/applications is a applications page, its that simple.</p>
+            </div>
+            <div class="buttons"><a class="btn btn-primary" role="button" href="https://dinoman.dev/applications">Applications</a></div>
+        </div>
+    </div>
+    <div data-aos="fade-up" data-aos-once="true" class="highlight-blue" style="background: #222222;">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Cloud Storage</h2>
+                <p class="text-center" style="color: rgb(255,255,255);">This is beta cloud storage, you can sign up by contacting me on discord.</p>
+            </div>
+            <div class="buttons"><a class="btn btn-primary" role="button" href="https://dinoman.dev/cloud-storage">Cloud Storage</a></div>
+        </div>
+    </div>
+    <script src="/assets/js/jquery.min.js?h=89312d34339dcd686309fe284b3f226f"></script>
+    <script src="/assets/bootstrap/js/bootstrap.min.js?h=03ab36d1dde930b7d44a712f19075641"></script>
+    <script src="/assets/js/bs-init.js?h=2e4f94618d12c613ae5f1a99ad96090e"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+    <script src="/assets/js/Swiper-Slider-Card-For-Blog-Or-Product.js?h=991afcfa41cba2f581d9aab17dc1d26a"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Dinoman.dev</title>
+    <meta name="description" content="Welcome, Come on in.">
+    <link rel="icon" type="image/png" sizes="400x352" href="/assets/img/logo2.png?h=6ad3b23f7a0be358783adb294da3d091">
+    <link rel="icon" type="image/png" sizes="400x352" href="/assets/img/logo2.png?h=6ad3b23f7a0be358783adb294da3d091">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css?h=4f10ccc55b166919047815d3c65de7fb">
+    <link rel="stylesheet" href="/assets/fonts/font-awesome.min.css?h=9db842b3dc3336737559eb4abc0f1b3d">
+    <link rel="stylesheet" href="/assets/css/Animated-CSS-Waves-Background-SVG.css?h=663274753fd79fac442fe55a227b70e7">
+    <link rel="stylesheet" href="/assets/css/Article-List.css?h=cec575f25bba55cfaa10100750e76412">
+    <link rel="stylesheet" href="/assets/css/Contact-Form-Clean.css?h=c942239c91f94a6b90d67fb2496c21bf">
+    <link rel="stylesheet" href="/assets/css/gradient-navbar-1.css?h=b3bbc83d97bd59482a9960d0ecc55a15">
+    <link rel="stylesheet" href="/assets/css/gradient-navbar.css?h=b3bbc83d97bd59482a9960d0ecc55a15">
+    <link rel="stylesheet" href="/assets/css/Highlight-Blue.css?h=91a04f2b18410bc09760f3473788176a">
+    <link rel="stylesheet" href="/assets/css/Highlight-Clean.css?h=22f5c37d9256feae966552e68b9ff2f3">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="/assets/css/styles.css?h=d41d8cd98f00b204e9800998ecf8427e">
+    <link rel="stylesheet" href="/assets/css/Swiper-Slider-Card-For-Blog-Or-Product-1.css?h=ffee04c5cb354382dfa8f41065ecb54c">
+    <link rel="stylesheet" href="/assets/css/Swiper-Slider-Card-For-Blog-Or-Product.css?h=35808ef06463b831c589a6910b5c8506">
+</head>
+
+<body style="background: #222222;color: rgb(255,255,255);">
+    <nav class="navbar navbar-dark navbar-expand-md" data-aos="fade-up" data-aos-once="true" id="app-navbar">
+        <div class="container-fluid"><a class="navbar-brand" href="https://dinoman.dev"><i class="fa fa-connectdevelop" id="brand-logo"></i></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link active" href="https://github.com/DarkMaTTTTTTer/">Github</a></li>
+                    <li class="nav-item"><a class="nav-link" href="https://dinoman.dev/Current-Projects.php">Current Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" href="https://dinoman.dev/Contact.php">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="highlight-blue" style="background: #222222;">
+        <div class="container" data-aos="fade-up">
+            <div class="intro">
+                <h2 class="text-center">Welcome!</h2>
+                <p class="text-center" style="color: rgb(255,255,255);">DInoman.dev is a development platform for all my test projects. Go to the current projects page to view all projects.</p>
+            </div>
+            <div class="buttons"><a class="btn btn-primary" role="button" data-aos="fade-up" data-aos-once="true" href="https://dinoman.dev/Current-Projects.php">CUrrent Projects</a></div>
+        </div>
+    </div>
+    <div class="article-list" style="background: #222222;">
+        <div class="container" data-aos="fade-up">
+            <div class="intro">
+                <h2 class="text-center" style="color: rgb(255,255,255);"><strong>Official Release Projects:</strong><br></h2>
+                <p class="text-center">Check out My Official Releases!</p>
+            </div>
+            <div class="row articles">
+                <div class="col-sm-6 col-md-4 item"><a href="https://dinoman.dev"><img class="img-fluid" src="/assets/img/logo2.png?h=6ad3b23f7a0be358783adb294da3d091"></a>
+                    <h3 class="name" style="color: rgb(255,255,255);">Dinoman.dev</h3>
+                    <p class="description">Dinoman.dev is a development platform that allows for real time testing.</p><a class="action" href="https://github.com/DarkMaTTTTTTer/"><i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+                <div class="col-sm-6 col-md-4 item"><a href="https://linuxnodes.net"><img class="img-fluid" src="/assets/img/LinuxNodes.net.png?h=a20b144734ef0e40cdf5ea2ba5f16357"></a>
+                    <h3 class="name" style="color: rgb(255,255,255);">LinuxNodes.net</h3>
+                    <p class="description">LinuxNodes.net is a free hosting service, owned and operated by your truly.</p><a class="action" href="https://linuxnodes.net"><i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+                <div class="col-sm-6 col-md-4 item"><a href="https://dinoman.dev/youfoundme"><img class="img-fluid" src="/assets/img/comming%20soon.png?h=c6db64a07861d1fba38af9dbee0e190a"></a>
+                    <h3 class="name" style="color: rgb(255,255,255);">Coming Soon...</h3>
+                    <p class="description">We will see....</p><a class="action" href="https://dinoman.dev/youfoundme"><i class="fa fa-arrow-circle-right" data-toggle="tooltip" data-bs-tooltip="" title="Shh, Nothing Here."></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div data-aos="fade-up" data-aos-once="true" class="highlight-clean" style="color: rgb(255,255,255);background: #222222;">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Discord/GitHub</h2>
+                <p class="text-center" style="color: rgb(255,255,255);">Check out my discord and my GitHub Page.</p>
+            </div>
+            <div class="buttons"><a class="btn btn-primary disabled" role="button" href="#">DarkerMatter#0001</a><a class="btn btn-light" role="button" href="https://github.com/DarkMaTTTTTTer/">GitHub</a></div>
+        </div>
+    </div>
+    <script src="/assets/js/jquery.min.js?h=89312d34339dcd686309fe284b3f226f"></script>
+    <script src="/assets/bootstrap/js/bootstrap.min.js?h=03ab36d1dde930b7d44a712f19075641"></script>
+    <script src="/assets/js/bs-init.js?h=2e4f94618d12c613ae5f1a99ad96090e"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+    <script src="/assets/js/Swiper-Slider-Card-For-Blog-Or-Product.js?h=991afcfa41cba2f581d9aab17dc1d26a"></script>
+</body>
+
+</html>
